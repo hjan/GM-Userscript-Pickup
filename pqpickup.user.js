81d4816a0c59975884a28c6f290811302cd62889
hjan <hoeppner.jan@xurv.org>
2013-08-07 23:53:15 +0200

    Added live information section to the upper right corner of the ql page (next to the news feeds) Added long poll function (not yet finished)
    Signed-off-by: hjan <hoeppner.jan@xurv.org>

--- pqpickup.user.js
+++ pqpickup.user.js
@@ -31,6 +31,16 @@ function contentEval(source) {
 	document.body.removeChild(script);
 }
 
+function pollLiveData(event) {
+
+	setTimeout(function() {
+		GM_xmlhttpRequest({
+			method: 'GET',
+			url : 'http://bot.xurv.org/teams.dev.php?id=',
+			
+		});
+	}, 500);
+}
 /*
  * This will handle the team-request-event send by /teams within QL
  */
@@ -47,7 +57,9 @@ function handleTeamRequest(event) {
 		return;
 	}
 
-    if ("PQ:teamRequest" == request.type || "PQ:mpRequest" == request.type) {
+    if ("PQ:teamRequest" == request.type 
+    	|| "PQ:mpRequest" == request.type
+    	|| "PQ:missRequest" == request.type) {
         GM_xmlhttpRequest({
             method : 'GET',
             url : 'http://bot.xurv.org/teams.dev.php?id=' + request.serverID,
@@ -62,6 +74,9 @@ function handleTeamRequest(event) {
                     }
                     if ("PQ:mpRequest" == request.type) {
                         response.type = "PQ:mpResponse";
+                    }
+                    if ("PQ:missRequest" == request.type) {
+                        response.type = "PQ:missResponse";
                     }
                 } catch (e) {
                     console.log("Couldn't parse requested data: " + e);
@@ -79,6 +94,17 @@ window.addEventListener("message", handleTeamRequest, false);
  */
 contentEval(function() {
 	PQT = {
+/*		 isStandardUser : function(player) {
+			GM_xmlhttpRequest({
+					synchronous : true,
+					method : 'GET',
+					url : '/profile/summary/' + player,
+					success : function(data) {
+						var pattern = new RegExp("premium_status_0", 'g');
+						return pattern.test(data);
+					}	
+				});
+		},*/
 		handleResponse : function(event) {
 			var response;
 			try {
@@ -98,6 +124,42 @@ contentEval(function() {
 				return;
 			}
 
+			if ("PQ:missResponse" == response.type) {
+				quakelive.serverManager.RefreshServerDetails(quakelive.currentServerId, {
+					onSuccess: function () {
+						var sv = quakelive.serverManager.GetServerInfo(quakelive.currentServerId);
+						
+						if (sv.players.length == 0) {
+							qz_instance.SendGameCommand('echo Could not retrieve any players from this server yet. Try again in a few seconds.;');
+							return;
+						}
+								
+						var p_players = response.teamRed.concat(response.teamBlue);
+						var missing_players = "^3Missing:^7 ";
+						var c = 0;
+						for (i in p_players) {
+//							console.log(p_players[i] + " needs an invite? " + PQT.isStandardUser(p_players[i]));
+							var found = false;
+							for (j in sv.players) {
+								if (p_players[i] == sv.players[j].name) {
+									found = true;
+									break;
+								} 
+							}
+							if (!found) {
+								c++;
+								missing_players += p_players[i] + " ";
+							}
+						}
+						if (!c) {
+							missing_players += "No one is missing. Pickup is ready to start.";
+						}
+						qz_instance.SendGameCommand('say ' + missing_players + ';');
+						return;
+					}
+				});
+			}
+			
             if ("PQ:mpResponse" == response.type) {
                 try {
                     var mapPicker = response.mapPicker;
@@ -106,6 +168,7 @@ contentEval(function() {
                     return;
                 }
                 qz_instance.SendGameCommand('say ^3Mappicker:^7 ' + mapPicker + ';');
+                return;
             }
             
             if ("PQ:teamResponse" == response.type) {
@@ -121,14 +184,14 @@ contentEval(function() {
                 var teamRString = "^1Red:^7 ";
                 var teamBString = "^4Blue:^7 ";
                 var tmp;
-                for ( var i = 0; i < teamR.length; i++) {
+                for (i in teamR) {
                     tmp = teamR[i];
                     if (teamR[i] == mapPicker) {
                         tmp = "^3" + teamR[i] + "^7";
                     }
                     teamRString += tmp + " ";				
                 }			
-                for ( var i = 0; i < teamB.length; i++) {
+                for (i in teamB) {
                     tmp = teamB[i];
                     if (teamB[i] == mapPicker) {
                         tmp = "^3" + teamB[i] + "^7";
@@ -139,10 +202,70 @@ contentEval(function() {
                 setTimeout(function(){
                     qz_instance.SendGameCommand('say ' + teamBString + ';');
                 }, 700);
+                return;
             }
 		}
 	};
 	window.addEventListener("message", PQT.handleResponse, false);
+});
+
+GM_addStyle(
+	"#ql_pickup_info {" +
+	"    width: 298px;" +
+	"    height: 75px;" +
+	"    text-align: left;" +
+	"    padding: 0;" +
+	"    margin: 0 0 10px 0;" +
+	"    font-weight: bold;" +
+	"	 background: #e7e7e7 url('http://cdn.quakelive.com/images/invites_title_v2013022600.0.png') no-repeat left top;" +
+	"    border: 1px solid rgb(57, 57, 57);" +
+	"}" +
+	".ql_pickup_head {" +
+	"    width: 290px;" +
+	"    height: 30px;" +
+	"    margin: 4px 0 0 9px;" +
+	"    font-size: 18px;" +
+	"    font-weight: bold;" +
+	"}" +
+	".ql_pickup_cnt span {" +
+	"    width: 290px;" +
+	"    color: #000;" +
+	"    margin: 0 0 0 9px;" +
+	"    font-size: 11px;" +
+	"    font-weight: normal;" +
+	"}"
+);
+
+contentEval(function() {
+	var $ui, $tb, intStatusTop, tries = 200;
+	intStatusTop = setInterval(function() {
+		$ui = $("#post_spon_content");
+		$tb = $ui.find("#my_server");
+		
+		if (--tries && !$tb.length) {
+			return;
+		}
+		clearInterval(intStatusTop);
+		intStatusTop = null;
+
+		$tb.before("" +
+				"<div id='ql_pickup_info'>" +
+				"<div class='ql_pickup_head'>Pickup Live Info</div>" +
+				"<div class='ql_pickup_cnt'>" +
+				" <span>Players: 3/8</span><br />" +
+				" <span>Server: Link</span>" +
+				"</div>" +
+				"</div>" +
+				"");
+		
+		// once we've added our section, poll for pickup data
+		var msg = {
+			"type" : "PQ:liveDataRequest",
+			"user" : quakelive.username
+		};
+		window.postMessage(JSON.stringify(msg), "*");
+		
+	}, 100);
 });
 
 /*
@@ -180,21 +303,34 @@ contentEval(function() {
                     }), "*");
                 }
             }
+        },
+        missing : {
+            params : false,
+            dft : 0,
+            fn : function(val) {
+                if (!quakelive.currentServerId) {
+                    qz_instance.SendGameCommand("echo Pickup with Bots? Good luck with that...;");
+                } else {
+                    window.postMessage(JSON.stringify({
+                        "serverID" : quakelive.currentServerId,
+                        "type" : "PQ:missRequest"
+                    }), "*");
+                }
+            }
         }
 
 	};
 	var oldLaunchGame = LaunchGame, ready;
 	LaunchGame = function(params, server) {
 		ready = false;
-		var i;
 		for (i in commands) {
 			if (commands[i].params) {
 				params.Append('+set ' + i + ' "^7"');
 				params.Append('+set ' + i + ' "' + commands[i].dft + '"');
 			} else {
 				commands[i].dft = 0;
-				params.Append('+set GM_qlfc_' + i + ' "0"');
-				params.Append('+alias ' + i + ' "set GM_qlfc_' + i + ' 1"');
+				params.Append('+set GM_qlpqc_' + i + ' "0"');
+				params.Append('+alias ' + i + ' "set GM_qlpqc_' + i + ' 1"');
 			}
 		}
 		return oldLaunchGame.apply(this, arguments);
@@ -211,10 +347,9 @@ contentEval(function() {
 	};
 	var oldOnCvarChanged = OnCvarChanged;
 	OnCvarChanged = function(name, value, replicate) {
-		var i;
 		for (i in commands) {
 			if ((commands[i].params && name == i)
-					|| (!commands[i].params && name == 'GM_qlfc_' + i)) {
+					|| (!commands[i].params && name == 'GM_qlpqc_' + i)) {
 				if (value != commands[i].dft) {
 					if (ready) {
 						commands[i].fn(value);
