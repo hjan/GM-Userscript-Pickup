15f4e33c1b5aefbe5d80a355f66ca38639d2eb1b
hjan <hoeppner.jan@xurv.org>
2013-11-14 01:54:53 +0100

    Added Update Function Added Chrome specific stuff
    Signed-off-by: hjan <hoeppner.jan@xurv.org>

--- ipis.user.js
+++ ipis.user.js
@@ -24,6 +24,9 @@ var SCRIPT_NAME = "Integrated Pickup Information System",
 	SCRIPT_VER	= "0.9",
 	GM_updatingEnabled = "GM_updatingEnabled" in window ? GM_updatingEnabled : false;
 
+var DOLITTLE = function () {};
+var isChrome = /chrome/i.test(navigator.userAgent);
+
 var API_URL = "http://api.ctfpickup.eu:23007/";
 var IRC_CHANNEL = "ctfpickup";
 
@@ -62,6 +65,44 @@ GM_registerMenuCommand(SCRIPT_NAME + "Toggle Information Box", function() {
 },
 "i");
 
+// Don't bother if we're missing critical GM_ functions
+if ("undefined" == typeof(GM_xmlhttpRequest)) {
+	alert("Your browser/add-on doesn't support GM_xmlhttpRequest, which is "
+			+ "required for " + SCRIPT_NAME + " to operate.");
+}
+
+/**
+ * We can work around other missing GM_ functions using a modified version of...
+ *
+ * GM_ API emulation for Chrome; 2009, 2010 James Campos
+ * cc-by-3.0; http://creativecommons.org/licenses/by/3.0/
+ */
+if (isChrome || "undefined" == typeof GM_getValue) {
+	GM_getValue = function(aName, aDefaultValue) {
+		var value = localStorage.getItem(aName);
+		if (!value) return aDefaultValue;
+		var type = value[0];
+		value = value.substring(1);
+		switch (type) {
+			case "b":
+				return value == "true";
+			case "n":
+				return Number(value);
+			default:
+				return value;
+		}
+	};
+
+	GM_setValue = function(aName, aValue) {
+		aValue = (typeof aValue)[0] + aValue;
+		localStorage.setItem(aName, aValue);
+	};
+}
+
+if (isChrome || "undefined" == typeof GM_registerMenuCommand) {
+	GM_registerMenuCommand = DOLITTLE;
+}
+
 
 /**
  * Use an auto-update script if integrated updating isn't enabled
@@ -72,9 +113,101 @@ GM_registerMenuCommand(SCRIPT_NAME + "Toggle Information Box", function() {
  * NOTE: Taken from http://userscripts.org/scripts/show/111519
  */
 if (!GM_updatingEnabled) {
-  var AutoUpdater_166645={id:166645,days:1,name:SCRIPT_NAME,version:SCRIPT_VER,time:new Date().getTime(),call:function(response,secure){GM_xmlhttpRequest({method:"GET",url:"http"+(secure?"s":"")+"://userscripts.org/scripts/source/"+this.id+".meta.js",onload:function(xpr){AutoUpdater_166645.compare(xpr,response)},onerror:function(xpr){if(secure){AutoUpdater_166645.call(response,false)}}})},enable:function(){GM_registerMenuCommand(this.name+": Enable updates",function(){GM_setValue("updated_166645",new Date().getTime()+"");AutoUpdater_166645.call(true,true)})},compareVersion:function(r_version,l_version){var r_parts=r_version.split("."),l_parts=l_version.split("."),r_len=r_parts.length,l_len=l_parts.length,r=l=0;for(var i=0,len=(r_len>l_len?r_len:l_len);i<len&&r==l;++i){r=+(r_parts[i]||"0");l=+(l_parts[i]||"0")}return(r!==l)?r>l:false},compare:function(xpr,response){this.xversion=/\/\/\s*@version\s+(.+)\s*\n/i.exec(xpr.responseText);this.xname=/\/\/\s*@name\s+(.+)\s*\n/i.exec(xpr.responseText);if((this.xversion)&&(this.xname[1]==this.name)){this.xversion=this.xversion[1];this.xname=this.xname[1]}else{if((xpr.responseText.match("the page you requested doesn't exist"))||(this.xname[1]!=this.name)){GM_setValue("updated_166645","off")}return false}var updated=this.compareVersion(this.xversion,this.version);if(updated&&confirm("A new version of "+this.xname+" is available.\nDo you wish to install the latest version ("+this.xversion+")?")){var path="http://userscripts.org/scripts/source/"+this.id+".user.js";if(window.chrome){prompt("This script can't be updated automatically in Chrome.\nPlease uninstall the old version, and navigate to the URL provided below.",path)}else{try{window.parent.location.href=path}catch(e){}}}else{if(this.xversion&&updated){if(confirm("Do you want to turn off auto updating for this script?")){GM_setValue("updated_166645","off");this.enable();if(window.chrome){alert("You will need to reinstall this script to enable auto-updating.")}else{alert("Automatic updates can be re-enabled for this script from the User Script Commands submenu.")}}}else{if(response){alert("No updates available for "+this.name)}}}},check:function(){if(GM_getValue("updated_166645",0)=="off"){this.enable()}else{if(+this.time>(+GM_getValue("updated_166645",0)+1000*60*60*24*this.days)){GM_setValue("updated_166645",this.time+"");this.call(false,true)}GM_registerMenuCommand(this.name+": Check for updates",function(){GM_setValue("updated_166645",new Date().getTime()+"");AutoUpdater_166645.call(true,true)})}}};AutoUpdater_166645.check();
+	var AutoUpdater_166645 = {
+		id: 166645,
+		days: 1,
+		name: SCRIPT_NAME,
+		version: SCRIPT_VER,
+		time: new Date().getTime(),
+		call: function (response, secure) {
+			GM_xmlhttpRequest({
+				method: "GET",
+				url: "http" + (secure ? "s" : "") + "://userscripts.org/scripts/source/" + this.id + ".meta.js",
+				onload: function (xpr) {
+					AutoUpdater_166645.compare(xpr, response);
+				},
+				onerror: function (xpr) {
+					if (secure) {
+						AutoUpdater_166645.call(response, false);
+					}
+				}
+			});
+		},
+		enable: function () {
+			GM_registerMenuCommand(this.name + ": Enable updates", function () {
+				GM_setValue("updated_166645", new Date().getTime() + "");
+				AutoUpdater_166645.call(true, true);
+			});
+		},
+		compareVersion: function (r_version, l_version) {
+			var r_parts = r_version.split("."),
+				l_parts = l_version.split("."),
+				r_len = r_parts.length,
+				l_len = l_parts.length,
+				r = l = 0;
+			for (var i = 0, len = (r_len > l_len ? r_len : l_len); i < len && r == l; ++i) {
+				r = +(r_parts[i] || "0");
+				l = +(l_parts[i] || "0");
+			}
+			return (r !== l) ? r > l : false;
+		},
+		compare: function (xpr, response) {
+			this.xversion = /\/\/\s*@version\s+(.+)\s*\n/i.exec(xpr.responseText);
+			this.xname = /\/\/\s*@name\s+(.+)\s*\n/i.exec(xpr.responseText);
+			if ((this.xversion) && (this.xname[1] == this.name)) {
+				this.xversion = this.xversion[1];
+				this.xname = this.xname[1];
+			} else {
+				if ((xpr.responseText.match("the page you requested doesn't exist")) || (this.xname[1] != this.name)) {
+					GM_setValue("updated_166645", "off");
+				}
+				return false;
+			}
+			var updated = this.compareVersion(this.xversion, this.version);
+			if (updated && confirm("A new version of " + this.xname + " is available.\nDo you wish to install the latest version (" + this.xversion + ")?")) {
+				var path = "http://userscripts.org/scripts/source/" + this.id + ".user.js";
+				if (window.chrome) {
+					prompt("This script can't be updated automatically in Chrome.\nPlease uninstall the old version, and navigate to the URL provided below.", path);
+				} else {
+					try {
+						window.parent.location.href = path;
+					} catch (e) {}
+				}
+			} else {
+				if (this.xversion && updated) {
+					if (confirm("Do you want to turn off auto updating for this script?")) {
+						GM_setValue("updated_166645", "off");
+						this.enable();
+						if (window.chrome) {
+							alert("You will need to reinstall this script to enable auto-updating.");
+						} else {
+							alert("Automatic updates can be re-enabled for this script from the User Script Commands submenu.");
+						}
+					}
+				} else {
+					if (response) {
+						alert("No updates available for " + this.name);
+					}
+				}
+			}
+		},
+		check: function () {
+			if (GM_getValue("updated_166645", 0) == "off") {
+				this.enable();
+			} else {
+				if (+this.time > (+GM_getValue("updated_166645", 0) + 1000 * 60 * 60 * 24 * this.days)) {
+					GM_setValue("updated_166645", this.time + "");
+					this.call(false, true);
+				}
+				GM_registerMenuCommand(this.name + ": Check for updates", function () {
+					GM_setValue("updated_166645", new Date().getTime() + "");
+					AutoUpdater_166645.call(true, true);
+				});
+			}
+		}
+	};
+	AutoUpdater_166645.check();
 }
-
 
 function pollLiveData(event) {
 	var request;
